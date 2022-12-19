d4fffca98b602af2fdb4f74134c6b575e77e3149
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2022-12-19 10:44:02 +0100

    zadanie 1

--- main.py
+++ main.py
@@ -1,1 +1,61 @@
-#test
+# test
+# print('dupa')
+# print('dupa')
+# print('dupa')
+# print('dupa')
+# print('dupa')
+
+# przerwa do 10:16
+#
+# print('hello world!')
+# print("hello world!")
+# print("hello Mc'Donalds")
+# #print 'dupa' #wersja 2, juz nie dziala #fuuuuuuu
+# imie='Andrzej'
+# print(imie)
+# print(type(imie))
+# imie=123
+# print(imie)
+#print('')
+
+# imie="Andrzej"
+# print('Siema, jestem '+imie+"!")
+# x=10.5
+# print(type(x))
+# print("Siema, x wynosi "+x+"!")
+#
+# x=10.5
+# y=2400
+# #print("x="+str(x)+'!')
+# print("x={} a y={}!".format(x,y))
+# print(x,y)
+# print(type(x))
+# print(type(y))
+# print(type(x),type(y))
+# print("type(x)={} type(y)={}".format(type(x),type(y)))
+
+#
+# x=10.5
+# y=2400
+# print(f"x={x}, y={y}!")
+# print(f"x={x} y={y}")
+
+# x=input("Dej iksa:\n")
+# print(x,type(x))
+# #print(x/10)
+# y=float(x)
+# print(y,type(y))
+# print(y/10)
+# print(float(x)/10)
+#
+# x=1
+# if x>0:
+#     print('tak iks jezd wiekży od zera')
+# else:
+#     print('igz nie jezd wiekży od zera')
+# print('koniec')
+
+#print('whatever...')
+
+#1. Napisz program który przyjmie od użyszkodnika imię oraz nazwisko, a następnie
+# wypisze na konsoli komunikat typu "Witaj TwojeImie TwojeNazwisko!"

0117457d0441d4a16723fcec7b1944cec0016143
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2022-12-19 15:37:15 +0100

    zadanie

--- main.py
+++ main.py
@@ -574,3 +574,6 @@
 #17. Korzystając z list składanych wygeneruj listę 10 elementow której
 # każdy element również będzie listą. Pierwszy element tej podlisty to numer potegi,
 # a drugi to wartosc tej potegi dla liczby 2
+#
+# lista=[ [e,pow(2,e)] for e in range(1,11)]
+# print(lista)
